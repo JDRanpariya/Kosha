ac5c27d750a6075c9088be9f17bc4d92736bd1dd
JDRanpariya <jdpatel.code@gmail.com>
2026-03-21 10:42:58 +0100

    init tests

---
+++ tests/backend/db/check_db.py
@@ -0,0 +1,13 @@
+# backend/check_db.py
+from sqlalchemy import inspect
+from database import engine
+
+# Connect to the database and inspect it
+inspector = inspect(engine)
+
+# Get the names of all the tables
+tables = inspector.get_table_names()
+
+print("Tables currently in the database:")
+for table in tables:
+    print(f"- {table}")
